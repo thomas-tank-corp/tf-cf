c550d19104d3cf85db9a94caffbef1cc80e0dd1c
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-10-27 10:18:33 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.24"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region     = "eu-west-2"
+  access_key = var.credentials.access_key
+  secret_key = var.credentials.secret_key
+}
+
+
+
+variable "credentials" {
+  description = "The credentials for connecting to AWS."
+  type = object({
+    access_key = string
+    secret_key = string
+  })
+  sensitive = true
+}
+
+
+
+resource "aws_cloudformation_stack" "network" {
+  name = "networking-stack"
+
+  parameters = {
+    VPCCidr = "10.0.0.0/16"
+  }
+
+  template_body = <<STACK
+{
+  "Parameters" : {
+    "VPCCidr" : {
+      "Type" : "String",
+      "Default" : "10.0.0.0/16",
+      "Description" : "Enter the CIDR block for the VPC. Default is 10.0.0.0/16."
+    }
+  },
+  "Resources" : {
+    "myVpc": {
+      "Type" : "AWS::EC2::VPC",
+      "Properties" : {
+        "CidrBlock" : { "Ref" : "VPCCidr" },
+        "Tags" : [
+          {"Key": "Name", "Value": "Primary_CF_VPC"}
+        ]
+      }
+    }
+  }
+}
+STACK
+}
